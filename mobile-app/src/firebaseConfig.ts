184bf3aabff7762700d293a832637b7e9f6b63b2
natpl <natplon10@gmail.com>
2025-12-02 20:58:19 +0100

    firebase config update

--- mobile-app/src/firebaseConfig.ts
+++ mobile-app/src/firebaseConfig.ts
@@ -1,22 +1,22 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
 
 let app: any = null;
 export let auth: any = null;
 export let db: any = null;
 
 export function initializeFirebase() {
-  if (app) return;
-  const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID"
-  };
-  app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  db = getFirestore(app);
+    if (app) return;
+    const firebaseConfig = {
+        apiKey: "AIzaSyDMipnTI4UsYmsQldbXd1WSigQl8ozR8j8",
+        authDomain: "kids-to-do-app-2ada7.firebaseapp.com",
+        projectId: "kids-to-do-app-2ada7",
+        storageBucket: "kids-to-do-app-2ada7.firebasestorage.app",
+        messagingSenderId: "264121194056",
+        appId: "1:264121194056:web:6704e14b99bb4feeff2a7d"
+    };
+    app = initializeApp(firebaseConfig);
+    auth = getAuth(app);
+    db = getFirestore(app);
 }
