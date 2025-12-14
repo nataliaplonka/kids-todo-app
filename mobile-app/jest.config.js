99221f1b95ea38a3cf7d8a7ea34652b92725b209
natpl <natplon10@gmail.com>
2025-12-14 20:01:53 +0100

    jest config update

--- mobile-app/jest.config.js
+++ mobile-app/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'jest-expo',
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native\\\\/polyfills)/)'
+    'node_modules/(?!(react-native|@react-native|@react-navigation)/)'
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect']
 };
