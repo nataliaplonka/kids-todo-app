e41eabf64dbef5f6b8b7ef3a83ad4f2ab0cb6870
natpl <natplon10@gmail.com>
2025-12-21 20:11:47 +0100

    jest config update

--- mobile-app/jest.config.js
+++ mobile-app/jest.config.js
@@ -1,7 +1,6 @@
-// jest.config.js
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-polyfills/.*)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
   ],
 };
